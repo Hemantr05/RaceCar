40383f1acf814975a4f63ce53b61fab9166f2da3
hemantr05 <basketballlife600@gmail.com>
2021-02-04 04:04:19 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(name='RaceCar-v0',
     version='0.1',
-    install_requires=['gym']
+    install_requires=required
 )
