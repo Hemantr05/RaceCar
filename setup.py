5423127b7fdea7eea9cc9b13c859ffd207b2a034
Hemant Rakesh <basketballlife600@gmail.com>
2021-03-28 23:59:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='RaceCar-v0',
-    version='0.1',
-    install_requires=required
+      version='0.1',
+      install_requires=required,
+      description='Custom car racing environments for OpenAI Gym',
+      url='https://github.com/Hemantr05/RaceCar',
+      author='Hemant Rakesh',
+      author_email='hemantrak05@gmail.com',
+      
 )
